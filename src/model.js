eb72ea6398e81ffc5a380421e04ef637bc0109f0
SergeyYakimov <teriertis@yandex.ru>
2020-10-03 18:23:38 +0800

    Made modules

---
+++ src/model.js
@@ -0,0 +1,20 @@
+export const model = [
+    {
+        type: 'title',
+        value: 'Title From JS'
+    },
+    {
+        type: 'text',
+        value: 'some text'
+    },
+    {
+        type: 'columns',
+        value: [
+            '123', '456', '789'
+        ]
+    }, 
+    {
+        type: 'image',
+        value: './assets/image.png'
+    }
+];
