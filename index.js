d41b37aaa23ad59963aa0b1ccbc845205305e494
SergeyYakimov <teriertis@yandex.ru>
2020-10-03 17:36:53 +0800

    Made html for display

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const model = [
         value: [
             '123', '456', '789'
         ]
+    }, 
+    {
+        type: 'image',
+        value: './assets/image.png'
     }
 ];
 
@@ -21,24 +25,52 @@ model.forEach(block => {
     let html = '';
 
     if (block.type === 'title') {
-        html = `
-            <div class="row">
-                <div class="col-sm">
-                    <h1>${block.value}</h1>
-                </div>
-            </div>
-        `;
+        html = title(block);
     } else if (block.type === 'text') {
-        html = `
-            <div class="row">
-                <div class="col-sm">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit.</p>
-                </div>
-            </div>
-        `
+        html = text(block);
     } else if (block.type === 'columns') {
-
+        html = columns(block);
+    } else if (block.type === 'image') {
+        html = image(block);
     }
 
     $site.insertAdjacentHTML('beforeend', html);
 });
+
+function title(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `;
+}
+
+function text(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+        </div>
+    `;
+}
+
+function columns(block) {
+    const html = block.value.map(item => `<div class="col-sm">${item}</div>`);
+
+    return `
+        <div class="row">
+            ${html.join('')}
+        </div>
+    `;
+}
+
+function image(block) {
+    return `
+        <div class="row">
+            <img src="${block.value}" />
+        </div>
+    `
+}
