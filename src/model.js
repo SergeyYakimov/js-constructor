288fb8b1cde55956c8f78e8ea8f5fc55c640867f
SergeyYakimov <teriertis@yandex.ru>
2020-10-03 21:48:13 +0800

    Added Block class

--- src/model.js
+++ src/model.js
@@ -1,60 +1,46 @@
 import image from './assets/image.png';
+import {Block} from './classes/blocks';
 
 export const model = [
-    {
-        type: 'title',
-        value: 'Constructor Pure JS', 
-        options: {
-            tag: 'h2',
-            styles: {
-                background: 'linear-gradient(to right, #ff0099, #493240)',
-                color: '#fff',
-                'text-align': 'center',
-                padding: '1.5rem'
-            }
+    new Block('title','Constructor Pure JS',  {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '1.5rem'
         }
-    },
+    }),
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Главная картинка'
+    }),
+    new Block('columns', [
+        'Приложение на чистом JS без использования библиотек',
+        'Необходимо использование принципов SOLID и ООП',
+        'JavaScript - это просто, интересно.'
+    ],
     {
-        type: 'image',
-        value: image, 
-        options: {
-            styles: {
-                padding: '2rem 0',
-                display: 'flex',
-                'justify-content': 'center'
-            },
-            imageStyles: {
-                width: '500px',
-                height: 'auto'
-            },
-            alt: 'Главная картинка'
+        styles: {
+            'font-weight': 'bold',
+            color: '#fff',
+            padding: '2rem',
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)'
         }
-    },
-    {
-        type: 'columns',
-        value: [
-            'Приложение на чистом JS без использования библиотек',
-            'Необходимо использование принципов SOLID и ООП',
-            'JavaScript - это просто, интересно.' 
-        ],
-        options: {
-            styles: {
-                'font-weight': 'bold',
-                color: '#fff',
-                padding: '2rem',
-                background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)'
-            }
+    }),
+    new Block('text', 'Интересная информация', {
+        styles: {
+            'font-weight': 'bold',
+            padding: '1rem',
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)'
         }
-    },
-    {
-        type: 'text',
-        value: 'Интересная информация',
-        options: {
-            styles: {
-                'font-weight': 'bold',
-                padding: '1rem',
-                background: 'linear-gradient(to left, #f2994a, #f2c94c)'
-            }
-        }
-    } 
+    })
 ];
