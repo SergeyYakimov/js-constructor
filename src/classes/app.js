05ba4b4d5a85f62992829b3a4e453378cb54acc6
SergeyYakimov <teriertis@yandex.ru>
2020-10-04 00:47:37 +0800

    Made App class

---
+++ src/classes/app.js
@@ -0,0 +1,23 @@
+import { Site } from './site';
+import { Sidebar } from './sidebar';
+
+export class App {
+    constructor(model) {
+        this.model = model;
+
+        this.init();
+    }
+
+    init() {
+        const site = new Site('#site');
+
+        const updateCallback = newBlock => {
+            this.model.push(newBlock);
+            site.render(this.model);
+        }
+
+        new Sidebar('#panel', updateCallback);
+
+        site.render(this.model);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import { model } from './model';
-import { Site } from './classes/site';
-import { Sidebar } from './classes/sidebar';
+import {App} from './classes/app';
 import './styles/main';
 
-const site = new Site('#site');
-
-const updateCallback = newBlock => {
-    model.push(newBlock);
-    site.render(model);
-}
-
-new Sidebar('#panel', updateCallback);
-
-site.render(model);
+new App(model);
